b18d85540c64546c8c8b1d1817e2962b87b5681a
Brent Ransom <sadumedusa@gmail.com>
2019-02-10 01:57:37 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,10 +6,10 @@ class Dog
   @@all = []
 
   def initialize(name, breed, age)
-@name = name
-@breed = breed
-@age = age
+    @name = name
+    @breed = breed
+    @age = age
 
-@@all << self
+    @@all << self
   end
 end
