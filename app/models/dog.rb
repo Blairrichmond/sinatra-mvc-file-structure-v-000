28c8309dc5ec15802edbb22123a26c8d2321154b
Brent Ransom <sadumedusa@gmail.com>
2019-02-10 01:56:54 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,11 @@
 
-class Dog(name, breed, age)
+class Dog
   
   attr_accessor :name, :breed, :age
   
   @@all = []
-def initi
+
+def initialize(name, breed, age)
   @name = name
   @breed = breed
   @age = age
